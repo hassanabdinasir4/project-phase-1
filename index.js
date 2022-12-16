9aa138b6308a54aa5ccfa0ca546a996d0080efe4
hassanabdinasir4 <hassan.hassan@student.moringaschool.com>
2022-12-16 12:01:22 +0300

    done

--- index.js
+++ index.js
@@ -4,3 +4,6 @@ const IMGPATH = `https://image.tmdb.org/t/p/w1280`;
 const SEARCHAPI = `https://api.themoviedb.org/3/search/movie?
 api_key=04c35731a5ee918f014970082a0088b1&query=`;
 const main = document.getElementById("content");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+getmovies(APIURL);
